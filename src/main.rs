5422714766135f9ecaa2c1f822e824b377f9b573
Zzrk <1252124617@qq.com>
2023-11-28 01:38:54 +0800

    feat: main guide

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
-use std::env;
+use rust_tools::Config;
+use std::io::{self, Write};
 use std::process;
 
-use rust_tools::Config;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    print!("1. Search keywords in the file.\n");
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let mut input = String::new();
+    while input.trim() != "1" {
+        print!("Please enter the number before to select the desired function:\n");
+        io::stdout().flush().unwrap();
+
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+    }
+
+    print!("Please enter the keywords you want to search:\n");
+    io::stdout().flush().unwrap();
+    let mut query = String::new();
+    io::stdin().read_line(&mut query).unwrap();
+
+    print!("Please enter the file path:\n");
+    io::stdout().flush().unwrap();
+    let mut file_path = String::new();
+    io::stdin().read_line(&mut file_path).unwrap();
+
+    let mut config: Vec<String> = Vec::new();
+    config.push(String::from("rust_tools"));
+    config.push(query.trim().to_string());
+    config.push(file_path.trim().to_string());
+    let config = Config::build(&config).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
+
+    // let args: Vec<String> = env::args().collect();
+
+    // let config = Config::build(&args).unwrap_or_else(|err| {
+    //     println!("Problem parsing arguments: {err}");
+    //     process::exit(1);
+    // });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
