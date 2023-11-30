259885b4e5f30ab5fe2ea726cbafd76038569343
Zzrk <1252124617@qq.com>
2023-11-30 23:57:36 +0800

    fix: debug panic

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,18 @@ use clap::Parser;
 use rust_tools::cli::{Cli, Commands};
 
 fn main() {
-    let cli = Cli::parse();
-
-    match &cli.command {
-        Commands::Grep(args) => {
-            if let Err(e) = args.run() {
-                println!("Application error: {}", e);
-                std::process::exit(1);
+    // use try_parse to avoid panic
+    match Cli::try_parse() {
+        Ok(cli) => match cli.command {
+            Commands::Grep(args) => {
+                if let Err(e) = args.run() {
+                    println!("Command Grep error: {}", e);
+                    std::process::exit(1);
+                }
             }
+        },
+        Err(e) => {
+            println!("Application error: {}", e);
         }
-    };
+    }
 }
