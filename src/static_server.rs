b9803128fdc8655d56b9402bbd236d7d8c9d7884
Zzrk <1252124617@qq.com>
2023-12-03 00:26:28 +0800

    feat: static_server ip option

--- src/static_server.rs
+++ src/static_server.rs
@@ -1,14 +1,15 @@
 use clap::Args;
-use get_if_addrs::{get_if_addrs, IfAddr};
+use get_if_addrs::get_if_addrs;
 use rocket::{fs::FileServer, Config};
 use std::error::Error;
+use std::net::IpAddr;
 use tokio::runtime::Runtime;
 
 #[derive(Args)]
 pub struct StaticServerArgs {
     /// Server root path, default current path
     path: Option<String>,
-    /// Server ip, default: 127.0.0.1
+    /// Server ip(prefix), default: 127.0.0.1
     #[arg(short, long)]
     ip: Option<String>,
     /// Server port, default: 8000
@@ -19,15 +20,15 @@ pub struct StaticServerArgs {
 impl StaticServerArgs {
     pub fn run(&self) -> Result<(), Box<dyn Error>> {
         let rt = Runtime::new()?;
+
         rt.block_on(async {
             let path = self.path.clone().unwrap_or(".".to_string());
-            let ip = self.ip.clone().unwrap_or_else(|| {
-                get_first_ip_starting_with_192().unwrap_or_else(|| "127.0.0.1".to_string())
-            });
+            let ip = get_first_ip_starting_with_prefix(self.ip.clone())
+                .unwrap_or("127.0.0.1".to_string());
             let port = self.port.unwrap_or(8000);
 
             let config = Config {
-                address: std::net::IpAddr::V4(ip.parse().unwrap()),
+                address: IpAddr::V4(ip.parse().unwrap()),
                 port,
                 ..Config::default()
             };
@@ -43,14 +44,19 @@ impl StaticServerArgs {
     }
 }
 
-fn get_first_ip_starting_with_192() -> Option<String> {
-    let if_addrs = get_if_addrs().unwrap();
-    for if_addr in if_addrs {
-        if let get_if_addrs::IfAddr::V4(ifv4_addr) = if_addr.addr {
-            if ifv4_addr.ip.to_string().starts_with("192") {
-                return Some(ifv4_addr.ip.to_string());
+fn get_first_ip_starting_with_prefix(ip_prefix: Option<String>) -> Option<String> {
+    match ip_prefix {
+        Some(ip_prefix) => {
+            let if_addrs = get_if_addrs().unwrap();
+            for if_addr in if_addrs {
+                if let get_if_addrs::IfAddr::V4(ifv4_addr) = if_addr.addr {
+                    if ifv4_addr.ip.to_string().starts_with(ip_prefix.as_str()) {
+                        return Some(ifv4_addr.ip.to_string());
+                    }
+                }
             }
+            None
         }
+        None => None,
     }
-    None
 }
