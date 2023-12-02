f2877c1ea2fcd2bd6386102271e2868debe75be1
Zzrk <1252124617@qq.com>
2023-12-03 02:24:00 +0800

    feat: format find result

--- src/find.rs
+++ src/find.rs
@@ -35,13 +35,24 @@ impl FindArgs {
                     let relative_path_str = relative_path.to_str().unwrap();
                     let full_path = file_path.canonicalize().unwrap();
                     let full_path_str = full_path.to_str().unwrap();
+                    // remove prefix \\?\ for windows
                     let full_path_without_prefix = if full_path_str.starts_with("\\\\?\\") {
                         full_path_str.strip_prefix("\\\\?\\").unwrap()
                     } else {
                         full_path_str
                     };
+
+                    // print header
+                    if count == 1 {
+                        println!(
+                            "{:<5} {:<20} {:<30} {}",
+                            "No", "File Name", "Relative Path", "Full Path"
+                        );
+                    }
+                    // print result
                     println!(
-                        "{count}.{file_name}    {relative_path_str}    {full_path_without_prefix}"
+                        "{:<5} {:<20} {:<30} {}",
+                        count, file_name, relative_path_str, full_path_without_prefix
                     );
                 }
             }
